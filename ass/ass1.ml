ab5ab7a633ec2669dd398c7ca4fe08c532440e50
SIM Kyu Doun <kdsim@csl2wk11.cse.ust.hk>
2020-09-27 21:05:49 +0800

    Ass1 Done.

--- ass/ass1.ml
+++ ass/ass1.ml
@@ -6,41 +6,68 @@ fun sumDigits(x: int) =
     else 
         x mod 10 + sumDigits(x div 10);
 
+(*sumDigits(0);
 sumDigits(1000);
-sumDigits(54321);
+sumDigits(54321);*)
+
+fun incrementList([], a) = [] | incrementList(n, a) =
+    (hd(n) + a)::incrementList(tl(n), a);
 
 fun matchingList([], y ) = [] | matchingList(x, y) =
     if hd(x) = y then
-        1::matchingList(tl(x),y)
+        1::matchingList(tl(x), y)
     else
         0::matchingList(tl(x), y);
 
-
-fun cumulativeList([], a) = [] | cumulativeList(x, a) =
-    if hd(x) = 1 then
-        map increment tl(x) 
-    else
-        hd(x)::cumulativeList(tl(x), a);
-
-fun increment x = x + 1;
-
+fun cumulSum([]) = [] | cumulSum(n) = 
+    hd(n)::incrementList(cumulSum(tl(n)), hd(n))
+ 
 fun frequencyPrefixSum([], y) = [] | frequencyPrefixSum(x, y) =
-    val a = matchingList(x, y)
-    cumulativeList(a, y);
-    
-frequencyPrefixSum([1. 2. 2. 4. 5]. 2);
+    cumulSum(matchingList(x,y));
+ 
+(*frequencyPrefixSum([1, 2, 2, 4, 5], 2);
 frequencyPrefixSum([1, 2, 2, 4, 5], 3);
-frequencyPrefixSum([], 2);
+frequencyPrefixSum([2, 1, 1, 1], 2);
+frequencyPrefixSum([], 2);*)
 
 
-fun flatten([]) = [] | flatten(a) = 
-    if hd(a) = [] then
-        ::[]
-    else
-        hd(a)::flatten(tl(a));
+datatype 'a llist = LList of 'a llist list | Elem of 'a;
 
-fun depth([]) = 0 | depth(a) =
+(*Elem(1);
+LList [];
+LList ([Elem(1), LList([Elem(2), LList([Elem 1, Elem(3)]), Elem(4)])]);
+
+val a = LList([Elem(1), Elem(2)]);*)
+
+fun flatten (Elem(x)) = x::[] | flatten (LList([])) = [] | flatten(LList(head::tail)) = flatten(head)@flatten(LList(tail));
+
+(*flatten(Elem(3));
+flatten(LList([]));
+flatten(LList([Elem(1), LList([Elem(2), LList([]), LList([]), Elem(3)]), Elem(4)]));*)
 
 
+fun depth(Elem(x)) = 0 | depth(LList([])) = 1 | depth(LList(head::tail)) = depth(head) + depth(LList(tail));
 
-fun 
+(*depth(Elem(1));
+depth(LList([]));
+depth(LList([Elem(1), LList([Elem(2), LList([]), Elem(3)]), Elem(4)]));*)
+
+
+fun equal(LList([]), LList([])) = true | equal(Elem a, Elem b) = (a = b) | equal(LList(a_hd::a_tl), LList(b_hd::b_tl)) = if (a_hd = b_hd) then equal(LList(a_tl), LList(b_tl)) else false;
+
+(*
+val test_1 = Elem(3);
+val test_2 = Elem(9);
+equal(test_1, test_2);
+
+val test_1 = Elem(3+6);
+val test_2 = Elem(9);
+equal(test_1, test_2);
+
+val test_3 = LList([Elem(#"1")]);
+val test_4 = LList([Elem(#"1")]);
+equal(test_3, test_4);
+
+val test_5 = LList([Elem(1), LList([Elem(2), LList([Elem(5), Elem(6)]), Elem(3)]), Elem(4)]);
+val test_6 = LList([Elem(1), LList([Elem(2), LList([Elem(5), LList([Elem(6)])]), Elem(3)]), Elem(4)]);
+equal(test_5, test_6);*)
