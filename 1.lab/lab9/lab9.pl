b87199fc5a9ed6d71707d065f04b940769f6f6d4
KyuDounSim <kdsim@csl2wk11.cse.ust.hk>
2020-11-09 11:25:22 +0800

    lab9 semi finished/

---
+++ 1.lab/lab9/lab9.pl
@@ -0,0 +1,8 @@
+%list_prefix([], Y).
+%list_prefix([A|Atail], [A|Btail]) :- list_prefix(Atail, Btail).
+%list_suffix(Atail, Atail).
+%list_suffix(Atail, [B|Btail]) :- list_suffix(Atail, Btail).
+
+list_prefix(Elem, List) :- append(Elem, _, List).
+list_suffix(Elem, List) :- append(_, Elem, List).
+list_prefix_suffix(X, Y) :- list_prefix(X,Y); list_suffix(X, Y).
