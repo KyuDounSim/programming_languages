b700adccadbcb2ed54af49a0afbb399323a4d4b4
KyuDounSim <simkyudoun@gmail.com>
2020-11-15 23:05:56 +0800

    ass3 Q1 rough solution.

--- 2.ass/ass3.pl
+++ 2.ass/ass3.pl
@@ -1,9 +1,30 @@
 /*SIM, Kyu Doun || kdsim || 20306527*/
 
-linear_regression(A, B).
+%Q1
+list_sum([], 0).
+list_sum([head|tail], sum) :-
+    list_sum(tail, remain),
+    sum is head + remain.
+
+/*length(?list, ?Int)*/
+list_square([], []).
+list_square([head|tail], append(head**2, list_square(tail, result), resultant)).
+
+list_pointwise([], [], []).
+list_pointwise([head1|tail1], [head2|tail2], append(head1 * head2, list_pointwise(tail1, tail2, resultant), resultant)).
+
+linear_regression(A, B) :-
+    A is list_sum(B) * list_sum(list_square(A)) - (list_sum(A) * list_sum(list_pointwise(A, B, resultant))),
+    B is length(list_pointwise(A, B)) * list_pointwise(A, B) - list_sum(A) * list_sum(B) / (length(A) * list_sum(list_square(A)) - list_sum(A)**2).
+%Q2
 interesting(X).
-atom_elements(name_of_atom, element_type, bonded_atoms).
+
+%Q3
 ch3(X).
+
+%Q4
 c6ring(X).
+
+%Q5
 tnt(X).
 
