b31b76df537ed831aea28f1438e4316897311523
KyuDounSim <simkyudoun@gmail.com>
2020-11-21 23:27:56 +0800

    A4 added.

---
+++ 2.ass/ass4/tnt_counting.cu
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "helpers.h"
+
+/* you can define data structures and helper functions here */
+
+
+/**
+ * please remember to set final_results and final_result_size 
+ * before return.
+ */
+void tnt_counting(int num_blocks_per_grid, int num_threads_per_block,
+        int* c_c, int* c_n, int* c_h, int* n_o,
+        int c_c_size, int c_n_size, int c_h_size, int n_o_size,
+        int* &final_results, int &final_result_size) {
+    
+}
