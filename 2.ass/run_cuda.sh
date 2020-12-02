6722020b79c1c38c3f2ed69574422f7ed4a8518d
KyuDounSim <kdsim@csl2wk11.cse.ust.hk>
2020-12-02 16:48:13 +0800

    A4 rough code added.

--- 2.ass/run_cuda.sh
+++ 2.ass/run_cuda.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash -e
 nvcc -std=c++11 -arch=compute_52 -code=sm_52 main.cu tnt_counting.cu -o tnt_counting
 num_block=8
 num_thread=512
--- 2.ass/tnt_counting.cu
+++ 2.ass/tnt_counting.cu
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "helpers.h"
-
+using namespace std;
 /* you can define data structures and helper functions here */
 
 
@@ -8,39 +8,73 @@
  * please remember to set final_results and final_result_size 
  * before return.
  */
+
+
+/*
+Input: C-C array, C-C array length
+Output: C-C array with c6 rings, C6ring array length
+*/
+
+__global__
+void find_c6ring(int *d_out, int *d_in, int& c_c_len) {
+    unsigned int d_hist_idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+    cout << d_hist_idx << endl;
+    if (d_hist_idx >= numElems)
+    {
+        return;
+    }
+
+    unsigned int cdf_val = 0;
+    for (int i = 0; i <= d_hist_idx; ++i)
+    {
+        cdf_val = cdf_val + d_in[i];
+    }
+    d_out[d_hist_idx] = cdf_val;
+}
+
+/*
+Input: n-o array
+Output: array of Nitrogen id's that have two Oxygen attached.
+
+__global__ void find_no2(int *d_out, int *d_in, int numElems) {
+    unsigned int d_hist_idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+    cout << d_hist_idx << endl;
+    if (d_hist_idx >= numElems)
+    {
+        return;
+    }
+
+    unsigned int cdf_val = 0;
+    for (int i = 0; i <= d_hist_idx; ++i)
+    {
+        cdf_val = cdf_val + d_in[i];
+    }
+    d_out[d_hist_idx] = cdf_val;
+}
+*/
+
 void tnt_counting(int num_blocks_per_grid, int num_threads_per_block,
         int* c_c, int* c_n, int* c_h, int* n_o,
         int c_c_size, int c_n_size, int c_h_size, int n_o_size,
         int* &final_results, int &final_result_size) {
 
-        int tid = blockDim.x * blockIdx.x + threadIdx.x;
-	int num_threads = blockDim.x * gridDim.x;
-
-        int i = 0;
-        for(i = 0; i < c_c_size; ++i) {
-                cout << c_c[i] << " ";
-        }
-
-        cout << endl;
-
-        i = 0;
-        for(i = 0; i < c_n_size; ++i) {
-                cout << c_n[i] << " ";
-        }
-
-        cout << endl;
+        int* d_c_c, *d_c_n, *d_c_h, *d_n_o;
         
-        i = 0;
-        for(i = 0; i < c_h_size; ++i) {
-                cout << c_h[i] << " ";
-        }
+        cudaMalloc((void **)&d_c_c, 2048 * sizeof(int));
+        cudaMalloc((void **)&d_c_n, 2048 * sizeof(int));
+        cudaMalloc((void **)&d_c_h, 2048 * sizeof(int));
+        cudaMalloc((void **)&d_n_o, 2048 * sizeof(int));
 
-        cout << endl;        
+        find_c6ring<<<num_blocks_per_grid, num_threds_per_block>>>(c_c, c_c_size);  
         
-        i = 0;
-        for(i = 0; i < n_o_size; ++i) {
-                cout << n_o[i] << " ";
-        }
+        int* c6_rings = (int*)malloc(num_blocks_per_grid * num_threds_per_block * sizeof(int));
+        cudaMemcpy(c6_rings, d_c_c, numb_blocks_per_grid * num_threads_per_blck * sizeof(int), cudaMemcpyDeviceToHost);
 
-        cout << endl;
-}
+        cudaFree(c6_rings);
+        free(d_c_c);
+        free(d_c_n);
+        free(d_c_h);
+        free(d_c_n);  
+} 
